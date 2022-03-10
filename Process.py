10cd6c29f785d914cffeccdb500effec266e1da0
MatthieuPourrat <50846358+MatthieuPourrat@users.noreply.github.com>
2022-03-10 17:21:21 -0500

    Base files and trials

---
+++ Process.py
@@ -0,0 +1,21 @@
+class Process:
+    def __init__(self, id, arrival_time,burst,priority):
+        self.id = id
+        self.arrival_time = arrival_time
+        self.burst = burst
+        self.priority = priority
+
+
+    def time_slot(self):
+        if self.priority < 100:
+            slot = (140 - self.priority) * 20 
+        else:
+            slot = (140 - self.priority) * 5
+        return slot            
+
+
+    def print(self):
+        print("id is: " + str(self.id))
+        print("Arrival time is: " + str(self.arrival_time))
+        print("burst: " + str(self.burst))
+        print("Priority is: " + str(self.priority))
---
+++ main.py
@@ -0,0 +1,11 @@
+from Process import Process
+
+
+
+queue1 = []
+queue2 = []
+
+object1 = Process("P1",1000,2500,90)
+object1.print()
+queue1.append(object1)
+print(queue1)
