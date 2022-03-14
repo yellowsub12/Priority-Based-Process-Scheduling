8254878cc57a782a4a38027d8b2e7cf2165c3b93
MatthieuPourrat <50846358+MatthieuPourrat@users.noreply.github.com>
2022-03-14 10:58:11 -0400

    Creating the processes from the file.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from Process import Process
 
 def read_file():
@@ -8,25 +9,28 @@ def read_file():
     open_file.close()
     return lines
 
-def create_processes(array):
+def create_processes(array, a):
     global nb_processes 
     nb_processes = array[0]
-    print(array)
-    count = 1
-    for i in array:
-        temp = array[count]
-        x = temp.split()
+    new_array = array[1:]
+    count = 0
+    for i in new_array:
+        i = new_array[count]
+        x = i.split()
         pid = x[0]
         arr_time = x[1]
         burst = x[2]
         priority = x[3]
         process = Process(pid,arr_time,burst,priority)
+        a.append(process)
         count = count + 1
-    return process
+        if count == 3:
+            break
+    return a
 
 
 queue1 = []
 queue2 = []
 pass1 = read_file()
-queue1.append(create_processes(pass1))
+print(str(create_processes(pass1, queue1)))
 print(queue1)
