dddc767a60b66ed6e1a5504cc177096cdbae702b
MatthieuPourrat <50846358+MatthieuPourrat@users.noreply.github.com>
2022-03-19 18:52:42 -0400

    Updates

--- Process.py
+++ Process.py
@@ -1,9 +1,10 @@
 class Process:
-    def __init__(self, id, arrival_time,burst,priority):
+    def __init__(self, id, arrival_time,burst,priority,nb_executions):
         self.id = id
         self.arrival_time = arrival_time
         self.burst = burst
         self.priority = priority
+        self.nb_excutions = nb_executions
 
 
     def time_slot(self):
@@ -11,6 +12,13 @@ class Process:
             slot = (140 - self.priority) * 0.020 
         else:
             slot = (140 - self.priority) * 0.005
+        return slot   
+
+    def time_slot(self, a):
+        if a < 100:
+            slot = (140 - a) * 0.020 
+        else:
+            slot = (140 - a) * 0.005
         return slot            
 
 
@@ -20,6 +28,26 @@ class Process:
         print("burst: " + str(self.burst))
         print("Priority is: " + str(self.priority))
 
+<<<<<<< Updated upstream
 
     def getID(self):
         return self.id
+=======
+    def getArrivalTime(self):
+        return self.arrival_time
+
+    def setNumberExecution(self):
+        self.nb_excutions = self.nb_excutions + 1
+
+    def setNumberExecution(self, a):
+        self.nb_excutions = a
+
+    def getNumberExecution(self):
+       return self.nb_excutions
+
+    def getBurst(self):
+        return self.burst
+
+    def setBurst(self, a):
+        self.burst = a
+>>>>>>> Stashed changes
